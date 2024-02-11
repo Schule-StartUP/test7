4cbee16b043c1b0daf3c4756a18ff2e4328f2a58
Schule-StartUP <159637304+Schule-StartUP@users.noreply.github.com>
2024-02-11 21:11:39 +0100

    Delete public/main.js

--- public/main.js
+++
@@ -1,9 +0,0 @@
-// Zuerst holst du dir eine Referenz auf den Button, den du auswählen möchtest
-const meinButton = document.querySelector('.meinButton');
-
-// Dann fügst du einen Eventlistener hinzu, der auf das Klicken des Buttons reagiert
-meinButton.addEventListener('click', function() {
-    // Hier kommt der Code, den du ausführen möchtest, wenn der Button geklickt wird
-    console.log('Der Button wurde geklickt!');
-    // Füge hier weitere Aktionen hinzu, die ausgeführt werden sollen
-});
