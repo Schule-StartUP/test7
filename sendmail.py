a49d6c5227d88fbc921f5a379059e08f78647bff
Schule-StartUP <159637304+Schule-StartUP@users.noreply.github.com>
2024-02-11 21:20:06 +0100

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -1,54 +1,42 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
-    # SMTP-Einstellungen für Outlook
-    smtp_server = "smtp.office365.com"
-    smtp_port = 587
 
-    # E-Mail-Adresse und App-Passwort
-    sender_email = "schule.startup@outlook.de"
-    sender_password = "Super197"
+# SMTP-Einstellungen für Outlook
+smtp_server = "smtp.office365.com"
+smtp_port = 587
 
-    # Empfänger-E-Mail-Adresse
-    receiver_email = "philippheine7g@gmail.com"
+# E-Mail-Adresse und App-Passwort
+sender_email = "schule.startup@outlook.de"
+sender_password = "Super197"
 
-    # Betreff und Nachricht
-    subject = "Test-E-Mail"
-    message_text = "Dies ist eine Test-E-Mail, die mit einem Python-Programm gesendet wurde."
+# Empfänger-E-Mail-Adresse
+receiver_email = "philippheine7g@gmail.com"
 
-    # Nachricht formatieren
-    message = MIMEMultipart()
-    message["From"] = sender_email
-    message["To"] = receiver_email
-    message["Subject"] = subject
+# Betreff und Nachricht
+subject = "Test-E-Mail"
+message_text = "Dies ist eine Test-E-Mail, die mit einem Python-Programm gesendet wurde."
 
-    # Textnachricht hinzufügen
-    message.attach(MIMEText(message_text, "plain"))
+# Nachricht formatieren
+message = MIMEMultipart()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
 
-    # Anhang hinzufügen (Beispiel: ein Bild mit dem Dateinamen "example.jpg")
-    attachment_path = r"C:\Mail Python\Businessplan_Vorlage.pdf"
+# Textnachricht hinzufügen
+message.attach(MIMEText(message_text, "plain"))
 
-    attachment = open(attachment_path, "rb")
+# Verbindung zum SMTP-Server herstellen
+with smtplib.SMTP(smtp_server, smtp_port) as smtp:
+    # Starten Sie die TLS-Verschlüsselung
+    smtp.starttls()
 
-    part = MIMEBase("application", "octet-stream")
-    part.set_payload(attachment.read())
-    encoders.encode_base64(part)
-    part.add_header("Content-Disposition", f"attachment; filename=Businessplan_Vorlage.pdf")
+    # Authentifizierung mit Benutzername und Passwort
+    smtp.login(sender_email, sender_password)
 
-    message.attach(part)
+    # E-Mail senden
+    smtp.sendmail(sender_email, receiver_email, message.as_string())
 
-    # Verbindung zum SMTP-Server herstellen
-    with smtplib.SMTP(smtp_server, smtp_port) as smtp:
-        # Starten Sie die TLS-Verschlüsselung
-        smtp.starttls()
-
-        # Authentifizierung mit Benutzername und Passwort
-        smtp.login(sender_email, sender_password)
-
-        # E-Mail senden
-        smtp.sendmail(sender_email, receiver_email, message.as_string())
 
  
   
