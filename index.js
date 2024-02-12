1f4af910bbb47e066ae5532a80f50d592bdf62c5
Schule-StartUP <159637304+Schule-StartUP@users.noreply.github.com>
2024-02-12 05:35:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-// Zuerst holst du dir eine Referenz auf den Button, den du auswählen möchtest
+import { Octokit } from "octokit";
+
 const meinButton = document.querySelector('.meinButton');
 
-// Dann fügst du einen Eventlistener hinzu, der auf das Klicken des Buttons reagiert
 meinButton.addEventListener('click', async function() {
     try {
-                const response = await fetch('https://api.github.com/repos/Schule-StartUP/test7/dispatches', {
-                    method: 'POST',
-                    headers: {
-                        'Authorization': `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`,
-                        'Accept': 'application/vnd.github.everest-preview+json',
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ event_type: 'trigger-workflow' })
-                });
+        const octokit = new Octokit({ 
+            auth: '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',
+        });
 
-                if (response.ok) {
-                    alert('GitHub Workflow wurde ausgelöst!');
-                } else {
-                    alert('Fehler beim Auslösen des GitHub Workflow.');
-                }
-            } catch (error) {
-                console.error('Error:', error);
-            }
+        const response = await octokit.request('POST /repos/Schule-StartUP/test7/dispatches', {
+            headers: {
+                'Accept': 'application/vnd.github.everest-preview+json',
+                'Content-Type': 'application/json'
+            },
+            mediaType: {
+                previews: ['everest']
+            },
+            event_type: 'trigger-workflow'
+        });
+
+        if (response.status === 204) {
+            alert('GitHub Workflow wurde ausgelöst!');
+        } else {
+            alert('Fehler beim Auslösen des GitHub Workflow.');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+
     console.log('Der Button wurde geklickt!');
     // Füge hier weitere Aktionen hinzu, die ausgeführt werden sollen
 });
